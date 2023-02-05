1c5d61d6387317af5aab44de48749240cdca2d5d
Agah Karakuzu <agahkarakuzu@gmail.com>
2023-02-04 19:37:37 -0500

    parse args through url

--- api/neurolibre_common_api.py
+++ api/neurolibre_common_api.py
@@ -1,12 +1,18 @@
-from flask import Response, Blueprint, abort, jsonify
+from flask import Response, Blueprint, abort, jsonify, request, current_app
 from common import *
 from flask_apispec import FlaskApiSpec, marshal_with, doc, use_kwargs
 from marshmallow import Schema, fields
+import logging
 
 common_api = Blueprint('common_api', __name__,
                         template_folder='./')
 
 #docs = FlaskApiSpec(common_api,document_options=False)
+
+gunicorn_error_logger = logging.getLogger('gunicorn.error')
+current_app.logger.handlers.extend(gunicorn_error_logger.handlers)
+current_app.logger.setLevel(logging.DEBUG)
+current_app.logger.debug('NeuroLibre common API.')
 
 @common_api.route('/api/heartbeat', methods=['GET'])
 @doc(description='Sanity check for the successful registration of the API endpoints.', tags=['Heartbeat'])
@@ -35,6 +41,15 @@ class BookSchema(Schema):
 def api_get_book(user_name=None,commit_hash=None,repo_name=None):
     
     if  not any([user_name, commit_hash, repo_name]):
+        current_app.logger.debug('No payload, parsing request arguments.')
+    
+    if "user_name" in request.args:
+        user_name = str(request.args['user_name'])
+    elif "commit_hash" in request.args:
+        commit_hash = str(request.args['commit_hash'])
+    elif "repo_name" in request.args:
+        repo_name = str(request.args['repo_name'])
+    else:
         abort(400)
 
     # Create an empty list for our results
