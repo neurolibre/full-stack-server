60a21a49d84c868700091acba3e6a0bfdf4bc09e
Agah <agahkarakuzu@gmail.com>
2023-05-02 00:42:32 -0400

    Enforce http1.1 for keepalive

--- api/wsgi_preview.py
+++ api/wsgi_preview.py
@@ -1,4 +1,6 @@
 from neurolibre_preview_api import app
+from werkzeug.serving import WSGIRequestHandler
 
 if __name__ == '__main__':
+    WSGIRequestHandler.protocol_version = "HTTP/1.1"
     app.run(debug=False)
