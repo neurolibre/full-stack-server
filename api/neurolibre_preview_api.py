6da371a22711c5dda3e12dfea9cd726eb6a45f72
Agah <agahkarakuzu@gmail.com>
2024-08-15 16:06:18 -0400

    dbg

--- api/neurolibre_preview_api.py
+++ api/neurolibre_preview_api.py
@@ -117,6 +117,8 @@ def api_download_data(user, id, repository_url, email, is_overwrite):
     issue_id = id
 
     task_title = "Download data for preview."
+    app.logger.debug(f'Here')
+    app.logger.debug(REVIEW_REPOSITORY)
     comment_id = gh_template_respond(github_client,"pending",task_title,REVIEW_REPOSITORY,issue_id)
 
     celery_payload = dict(repo_url=repository_url,
