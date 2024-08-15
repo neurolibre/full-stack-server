81f04050ff5f46f3b8c8ef7614052075a9e564d5
Agah <agahkarakuzu@gmail.com>
2024-08-15 15:40:06 -0400

    Try schema

--- api/schema.py
+++ api/schema.py
@@ -26,7 +26,7 @@ class DownloadSchema(Schema):
     Defines schema to be used for repo2data download. 
     """
     id = fields.Integer(required=False,description="Issue number of the technical screening of this preprint. If this used, the response will be returned to the respective github issue.")
-    repository_url = fields.Str(required=True,description="Full URL of a NeuroLibre compatible repository to be used for building the book.")
+    repository_url = fields.Str(required=False,description="Full URL of a NeuroLibre compatible repository to be used for building the book.")
     email = fields.Str(required=False,description="Email address, to which the result will be returned.")
     overwrite = fields.Boolean(required=False,description="Whether or not the downloaded data will overwrite, if already exists.")
 
