7004f80f715d91ee825c164cc752cbedd073f566
Agah <agahkarakuzu@gmail.com>
2024-12-11 20:18:06 -0500

    this should do it

--- api/neurolibre_preview_api.py
+++ api/neurolibre_preview_api.py
@@ -238,12 +238,11 @@ def api_myst_build_robo(user, repo_url, commit_hash, email):
     """
     This endpoint is used by robo.neurolibre.org.
     """
-    [owner, repo, _] =   get_owner_repo_provider(repo_url)
-    extra_payload = dict(is_prod=False)
+    [owner, repo, _] =  get_owner_repo_provider(repo_url)
+    extra_payload = dict(is_prod=False, commit_hash="latest", binder_hash=commit_hash)
     screening = ScreeningClient(task_name=f"{JOURNAL_NAME} build request for {owner}/{repo}", 
                                 email_address=email,
                                 target_repo_url=repo_url,
-                                commit_hash=commit_hash,
                                 **extra_payload)
     response = screening.start_celery_task(preview_build_myst_task)
     return response
