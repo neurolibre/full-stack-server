e716ed9c65605be01774e89c1af87c4dd2c250a8
agahkarakuzu <agahkarakuzu@gmail.com>
2025-09-25 21:57:51 +0300

    Address warning

--- api/neurolibre_celery_tasks.py
+++ api/neurolibre_celery_tasks.py
@@ -84,6 +84,7 @@ load_dotenv()
 celery_app = Celery('neurolibre_celery_tasks', backend='redis://localhost:6379/1', broker='redis://localhost:6379/0')
 
 celery_app.conf.update(task_track_started=True)
+celery_app.conf.broker_connection_retry_on_startup = True
 
 """
 Configuration END
